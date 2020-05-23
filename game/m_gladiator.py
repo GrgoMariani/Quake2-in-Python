1d15c192a8b7236d736043a52188ca5c008d7193
GrgoMariani <grimar2008@gmail.com>
2020-05-23 20:41:31 +0200

    game.m_gladiator.GladiatorGun

--- game/m_gladiator.py
+++ game/m_gladiator.py
@@ -2,9 +2,11 @@ from wrapper_qpy.decorators import TODO
 from wrapper_qpy.custom_classes import Mutable
 from .reference_import import gi
 from wrapper_qpy.linker import LinkEmptyFunctions
+from .m_flash import monster_flash_offset
+from shared.QConstants import MZ2_GLADIATOR_RAILGUN_1
 
 
-LinkEmptyFunctions(globals(), [])
+LinkEmptyFunctions(globals(), ["AngleVectors", "G_ProjectSource", "_VectorSubtract", "VectorNormalize"])
 
 
 @TODO
@@ -52,9 +54,16 @@ def gladiator_melee():
     pass
 
 
-@TODO
-def GladiatorGun():
-    pass
+def GladiatorGun(_self):
+    start = [0, 0, 0]
+    _dir = [0, 0, 0]
+    forward = [0, 0, 0]
+    right = [0, 0, 0]
+    AngleVectors(_self.s.angles, forward, right, None)
+    G_ProjectSource(_self.s.origin, monster_flash_offset[MZ2_GLADIATOR_RAILGUN_1], forward, right, start)
+    _VectorSubtract(_self.pos1, start, _dir)
+    VectorNormalize(_dir)
+    monster_fire_railgun(_self, start, _dir, 50, 100, MZ2_GLADIATOR_RAILGUN_1)
 
 
 @TODO
@@ -80,3 +89,8 @@ def gladiator_die():
 @TODO
 def SP_monster_gladiator():
     pass
+
+
+from .q_shared import AngleVectors, _VectorSubtract, VectorNormalize
+from .g_utils import G_ProjectSource
+from .g_monster import monster_fire_railgun
